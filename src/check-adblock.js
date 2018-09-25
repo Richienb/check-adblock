ff39a04075a6737d991195524ca1edc91d934564
Richie Bendall <richiebendall@gmail.com>
2018-09-25 23:59:51 +1200

    Update check-adblock.js

--- src/check-adblock.js
+++ src/check-adblock.js
@@ -1,1 +1,15 @@
+var init = function() {
+    var ads = require("./ads.js");
+}
 
+var check = function() {
+	if( window.canRunAds === undefined ){
+    	// adblocker detected
+        return false
+      } else {
+      	return true
+      }
+}
+
+module.exports.init = init;
+module.exports.check = check;
